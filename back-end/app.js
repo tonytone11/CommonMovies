7e36c7fa63d64a32c2ad1585d1644bd78e74cb2a
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-12-18 09:44:11 -0500

    404 page added

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { error } = require("console");
 const app = express();
 
-const Port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('', (req, res) => {
     res.render('/movie', {
@@ -11,6 +11,22 @@ app.get('', (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log("Server is running on" + PORT);
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anthony Montesdeoca',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Anthony Montesdeoca',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log("Server is running on" + port);
 })
