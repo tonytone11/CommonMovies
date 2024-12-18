11cc50f419d45752a99d00f6d5f423ad71afbdda
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-12-18 09:41:04 -0500

    added request function and get function

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,15 @@
 const path = require("path");
 const express = require("express");
+const { error } = require("console");
 const app = express();
 
 const Port = process.env.PORT || 3000;
+
+app.get('', (req, res) => {
+    res.render('/movie', {
+        error: "Not a real movie"
+    })
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on" + PORT);
--- back-end/utils/movies.js
+++ back-end/utils/movies.js
@@ -1,12 +1,25 @@
-const require = require("request");
+const request = require("request");
 
 function getMovies(movieSearch, callback) {
-    const url = 'https://api.themoviedb.org/3/search/movie?query=300&include_adult=false&language=en-US&page=1';
+    const url = 'https://api.themoviedb.org/3/search/movie?query=' + encodeURIComponent(movieSearch) + '&include_adult=false&language=en-US&page=1';
     const options = {
+        url,
+        json: true,
         method: 'GET',
         headers: {
             accept: 'application/json',
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWRjNThlN2Q1ODczZDM4ZWYzMTVhZDc2ZWNkNjBiZiIsIm5iZiI6MTczNDQ2MzM0OS4wNDcwMDAyLCJzdWIiOiI2NzYxY2Y3NWE3YWRlYWU3YWMyNTRlNGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.sfhQTU2znnK4Ib0k0n2UxZ-vHtr9uIc_SM3foDMIXBE'
         }
     };
+    request(options, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.statusCode != 200) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response)
+        }
+    })
 }
+
+module.exports = getMovies
